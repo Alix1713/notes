e5f162f025e943b032cb7df8e9db87dea94acc50
Alix17 <wildflowerinabox@gmail.com>
2021-11-14 16:07:48 -0800

    trying to fix repo so it displays

--- Express.js
+++ Express.js
@@ -8,10 +8,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index")));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 //* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
